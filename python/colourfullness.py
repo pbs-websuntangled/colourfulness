842daa65e93447a31c54e734adbc8d15adfd812b
Peter Brookes-Smith <Pbs.websuntangled@gmail.com>
2017-06-27 17:54:36 +0100

    Bug fixed. Ready to go

--- python/colourfullness.py
+++ python/colourfullness.py
@@ -1,13 +1,14 @@
-# This will analyse a picture and produce 2 derivatives of the picture that visualise the colourfullness of the image
-# The first derivative shows a greyscale image where the lightness is proprtional to the colourfullness
+# This will analyse a picture and produce 2 derivatives of the picture that visualise the colourfulness of the image
+# The first derivative shows a greyscale image where the lightness is proprtional to the colourfulness
 # the second derivative shows the original knocked back to grey and then dimmed to 0.25 with the 
-# colourfullness (also knocked back to 0.75 so no overflow) added onto the blue channel
-# it results in the original still being easily recognisable with the colourfull areas then being more blue
-# Inspired by and using the colourfullness calculation from Adrian at pyImageSearch 
-# in response to the blog article 
+# colourfulness (also knocked back to 0.75 so no overflow) added onto the blue channel
+# it results in the original still being easily recognisable with the colourful areas then being more red
+# Inspired by and using the colourfulness calculation from Adrian at pyImageSearch 
+# in response to the blog article http://www.pyimagesearch.com/2017/06/26/labeling-superpixel-colorfulness-opencv-python/
 
+def roicolourfulness(roi):
+    # thanks to Adrian for this function
 
-def roiColourfulness(roi):
 	# split the roi into its respective RGB components
 	(B, G, R) = cv2.split(roi.astype("float"))
  
@@ -30,19 +31,23 @@ def roiColourfulness(roi):
 
 import cv2
 import numpy as np 
+import time
 
+# start a timer because it's a long process!!
+startTime = time.time()
+
+# switch on or off the helpful print or save statements 
 debug = True
 
 # how much of the original to include in the second derivative image
-proportionOfOriginal = 0.3
+proportionOfOriginal = 0.25
 
-# read in a colourful picture
-imagePath = "../images/tests/colourfullness/20170617_185021.jpg"
-imagePath = "../images/tests/colourfullness/20170617_185021ShrunkMore.png"
-imagePath = "../images/tests/colourfullness/20170617_185021Shrunk.png"
-imagePath = "../images/tests/colourfullness/20170415_154428.jpg"
+# read in a colourful picture (choose one of the images below)
+imagePath = "../images/basilCatchingBall.jpg"
+imagePath = "../images/basilCatchingBallShrunk10.png"
+
 # where to save the pictures
-savePath = "/media/sf_localDesktop/debug"
+saveFolder = "../results/"
 
 # read it in unchanged
 imageColour = cv2.imread(imagePath)
@@ -51,54 +56,69 @@ imageColour = cv2.imread(imagePath)
 imageGrey = cv2.cvtColor(imageColour, cv2.COLOR_BGR2GRAY)
 cv2.equalizeHist(imageGrey, imageGrey) # spreads it out so 0 and 255
 
-# knock it  back to quarter
+# knock it  back 
 imageGreyHalf = np.zeros_like(imageGrey)
 imageGreyHalf[:] = imageGrey * proportionOfOriginal
 
-print("imageGreyHalf.shape", imageGreyHalf.shape)
+if debug:
+    print("imageGreyHalf.shape", imageGreyHalf.shape)
 
-# need to change the dtype here
-imageGreyHalfAsUint8 = imageGreyHalf.view(dtype=np.uint8)
-
-print("imageGreyHalfAsUint8.shape", imageGreyHalfAsUint8.shape)
-
-imageGreyForMarkup = cv2.cvtColor(imageGreyHalfAsUint8, cv2.COLOR_GRAY2BGR)
+# add some colour channels so that we can put a function of colourfulness in one of them later
+imageGreyForMarkup = cv2.cvtColor(imageGreyHalf, cv2.COLOR_GRAY2BGR)
 
 # get the basic dimensions
 height, width, channels = imageColour.shape
 
 # set the size of the window that we will roll over the image
-roiSize = min(height,width) / 20 # pixels for height and width of roi. Must be odd
-roiSize = int(roiSize / 2) * 2 + 1 # yes, it must be odd
+roiSize = min(height,width) / 10 # proportion of min image dimensions to have as roi size
+roiSize = int(roiSize / 2) * 2 + 1 # it must be odd
 halfRoiSize = int(roiSize / 2)
 
-# create a temporary home for the colourfulness index before I normalize it to 0-255 and put it back on the red channel
-colourfullnessChannel = np.zeros_like(imageGrey)
+# create a temporary home for the colourfulness index before I normalize it to 0-255 and put it back on a colour channel
+colourfulnessChannel = np.zeros_like(imageGrey)
 
-for rowIndex in range(height - halfRoiSize):
-    if rowIndex <= halfRoiSize:
-        continue
-    for pixelIndex in range(width - halfRoiSize):
-        if pixelIndex <= halfRoiSize:
-            continue
+# miss out the first and last bit where the roi doesn't fully cover the image
+for rowIndex in range(halfRoiSize, height - halfRoiSize):
+    for pixelIndex in range(halfRoiSize, width - halfRoiSize):
+
+        # define the region of interest that surrounds this pixel
         roi = imageColour[rowIndex - halfRoiSize: rowIndex + halfRoiSize, pixelIndex - halfRoiSize: pixelIndex + halfRoiSize]
 
-        colourfullnessindex = roiColourfulness(roi)
+        # use the function to calculate the colourfulness
+        colourfulnessindex = roicolourfulness(roi)
         
-        colourfullnessChannel[rowIndex, pixelIndex] = colourfullnessindex
+        # store it in the equivalent pixel position
+        colourfulnessChannel[rowIndex, pixelIndex] = colourfulnessindex
 
-cv2.equalizeHist(colourfullnessChannel, colourfullnessChannel) # spreads it out so 0 and 255
+# stretch the values
+cv2.equalizeHist(colourfulnessChannel, colourfulnessChannel) # spreads it out so 0 and 255
 
-cv2.imwrite(savePath + "/colourfullnessChannel.png", colourfullnessChannel)
+# save this out as it's the first derivative I desfribed above
+cv2.imwrite(saveFolder + "colourfulnessChannel.png", colourfulnessChannel)
 
-# knock it  back to half
-colourfullnessChannelHalf = np.zeros_like(colourfullnessChannel)
-colourfullnessChannelHalf[:] = colourfullnessChannel * (1 - proportionOfOriginal)
+# knock it  back to a fraction and add it to the original image
+colourfulnessChannelFraction = np.zeros_like(colourfulnessChannel)
+colourfulnessChannelFraction[:] = colourfulnessChannel * (1 - proportionOfOriginal)
 
+# decide which channel to put it on. Aesthetics are your guide here. Remember it's BGR not RGB 
+channelToUseForOverlay = 2
 
-imageGreyForMarkup[:, :, 0] = imageGreyForMarkup[:, :, 0] + colourfullnessChannelHalf
+# replace that channel with the one we created that is a function of the image and the colourfulness
+imageGreyForMarkup[:, :, channelToUseForOverlay] = imageGreyForMarkup[:, :, channelToUseForOverlay] + colourfulnessChannelFraction
 
-cv2.imwrite(savePath + "/imagePlusColourfullnessChannel.png", imageGreyForMarkup)
+# now let's boost the brightness up so it's not too dull
+maxBrightnessOfcolourfulnessChannel = np.max(imageGreyForMarkup[:, :, channelToUseForOverlay]) 
+brightnessBoostfactor = 255 / maxBrightnessOfcolourfulnessChannel
+imageGreyForMarkup[:, :, 0] = imageGreyForMarkup[:, :, 0] * brightnessBoostfactor
+imageGreyForMarkup[:, :, 1] = imageGreyForMarkup[:, :, 1] * brightnessBoostfactor
+imageGreyForMarkup[:, :, 2] = imageGreyForMarkup[:, :, 2] * brightnessBoostfactor
+
+# and save the second derivative out
+cv2.imwrite(saveFolder + "imagePluscolourfulnessChannel.png", imageGreyForMarkup)
+
+# We've finished so time it and print it out
+endTime = time.time()
+print("\nFinished. The run took this many seconds:", endTime - startTime, "\n")
 
 
 
